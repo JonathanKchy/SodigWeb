ee19d9cdabebc7cffdb28592d2b7c167caf7e67d
unknown <andres.jaag@hotmail.es>
2023-02-13 12:26:37 -0500

    finish class

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <h1>Hola</h1>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,13 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function AboutPage() {
   return (
     <>
       <div className={styles.container}>
       <Head>
         <title>SODIG</title>
-        <meta name="description" content="Home Page" />
+        <meta name="description" content="About Page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -18,12 +18,12 @@ export default function Home() {
         
           
           <h1 className={styles.title}>
-            Ir a <a href="/about">About</a>
+            Ir a <a href="/">Home</a>
           </h1>
           
           <p className={styles.description}>
             Get started by editing{' '};
-            <code className={styles.code}>src/pages/index.js</code>
+            <code className={styles.code}>src/pages/about.js</code>
           </p>
          
         
